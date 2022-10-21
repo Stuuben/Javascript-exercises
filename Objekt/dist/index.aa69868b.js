d1f0449d74df72da10e1b1f8a019b1e56af4d65a
Adam Stuborn <adam_stuborn@hotmail.com>
2022-10-21 08:32:28 +0100

    objekt

---
+++ Objekt/dist/index.aa69868b.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    //mallen
+    class Person {
+        eyeColor;
+        length;
+        age;
+    }
+    let me = new Person();
+    me.age = 34;
+    me.eyeColor = "Blue";
+    me.length = "182 cm";
+    let friend = new Person();
+    friend.age = 28;
+    friend.eyeColor = "brown";
+    friend.length = "170 cm";
+    let pTag = document.getElementById("pTag");
+    let people = [
+        me,
+        friend
+    ];
+    for(i = 0; i < people.lenght; i++)pTag.innerHTML += people[i].age;
+    console.log(me);
+    console.log(friend);
+};
+
+//# sourceMappingURL=index.aa69868b.js.map
